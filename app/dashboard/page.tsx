53a4534d80dcd7d7c523bd1b1ed0259e0cca42ad
VO BAO TRAN <tranvo.developer@gmail.com>
2025-11-18 17:42:34 +0000

    complete: chapter 7, fetching data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,31 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '@/app/lib/data';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { Card } from '@/app/ui/dashboard/cards';
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const {
+      numberOfCustomers,
+      numberOfInvoices,
+      totalPaidInvoices,
+      totalPendingInvoices
+    } = await fetchCardData();
+  return <main>
+    <p>Dashboard Page</p>
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card
+        title="Total Customers"
+        value={numberOfCustomers}
+        type="customers"
+      />
+    </div>
+    <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+      <RevenueChart revenue={revenue}  />
+      <LatestInvoices latestInvoices={latestInvoices} />
+    </div>
+  </main>
 }
